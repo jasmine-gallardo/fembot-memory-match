f42837d29344faf442209b811acf13b609b97a71
Jasmine Gallardo <j.m.gallardo6@gmail.com>
2020-04-24 14:53:34 -0700

    Changed cardFrontClassArray name to cardFrontClassNodeList to correct object type.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,8 @@ var matches = 0;
 var attempts = 0;
 var gamesPlayed = 0;
 
-var cardFrontClassArray = document.querySelectorAll(".card-front");
-var cardArray = Array.prototype.slice.call(cardFrontClassArray);
+var cardFrontClassNodeList = document.querySelectorAll(".card-front");
+var cardArray = Array.prototype.slice.call(cardFrontClassNodeList);
 
 function handleClick(event) {
   if (event.target.className.indexOf("card-back") === -1) {
